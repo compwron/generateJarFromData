3a298b9e0b8f0ae6faa92930c6a5d55bd48eb8e8
Linda Goldstein <lgoldste@thoughtworks.com>
2014-03-12 12:48:26 -0700

    cleaning up usage example

--- src/sampleusage/HierarchyUserExample.java
+++ src/sampleusage/HierarchyUserExample.java
@@ -14,19 +14,18 @@ public class HierarchyUserExample {
 
         ItemHierarchy itemHierarchy = new Hierarchy("test/presenter/data/sampleItemHierarchy.json").getItemHierarchy();
 
-//        Print data from json
-
         System.out.println("All Divisions in hierarchy: " + itemHierarchy.getDivisions());
         System.out.println("All Cls in hierarchy: " + new Collector().allCls(itemHierarchy));
         System.out.println("All Cls in division: " + new Collector().allCls(itemHierarchy.getDivisions().get(0)).size());
-        Division division1 = itemHierarchy.getDivisions().get(0);
-        Division division2 = itemHierarchy.getDivisions().get(1);
-        System.out.println("All Cls in one or multiple divisions: " + new Collector().allCls(division1, division2).size());
+        System.out.println("All departments in hierarchy: " + new Collector().allDepartments(itemHierarchy));
+        System.out.println("Division containing CLS with name: " + new Collector().findDivisionsByCls(itemHierarchy, new Cls("UNDETERMINABLES")));
 
         Department department = itemHierarchy.getDivisions().get(0).getDepartments().get(0);
         System.out.println("All Cls in department: " + new Collector().allCls(department));
+
+        Division division1 = itemHierarchy.getDivisions().get(0);
+        Division division2 = itemHierarchy.getDivisions().get(1);
+        System.out.println("All Cls in one or multiple divisions: " + new Collector().allCls(division1, division2).size());
         System.out.println("All departments in one or multiple divisions: " + new Collector().allDepartments(division1, division2));
-        System.out.println("All departments in hierarchy: " + new Collector().allDepartments(itemHierarchy));
-        System.out.println("Division containing CLS with name: " + new Collector().findDivisionsByCls(itemHierarchy, new Cls("UNDETERMINABLES")));
     }
 }
